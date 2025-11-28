d2ee95fac8109afc1993d0016b339e17091a6692
Miguel <foliaco18@outlook.es>
2025-11-28 17:27:54 -0500

    chore: :bookmark: change node version yaml

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,10 +1,1 @@
-import { webcrypto } from "node:crypto";
 import "./types/sato.d.ts";
-
-if (!globalThis.crypto?.getRandomValues) {
-    Object.defineProperty(globalThis.crypto, "getRandomValues", {
-        value: webcrypto.getRandomValues,
-        configurable: true,
-        writable: true,
-    });
-}
